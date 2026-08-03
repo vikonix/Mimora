a3c9af7d399569d2dee5ca1ac9380a79ecc794e6
vikonix <valeriy.kovalev.software@gmail.com>
2026-08-03 23:39:58 +0200

    fix name

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2026 Valery Kovalev
+# Copyright (c) 2026 Valeriy Kovalev
 """Mimora installer.
 
 A standalone, idempotent setup helper that walks through everything needed to
